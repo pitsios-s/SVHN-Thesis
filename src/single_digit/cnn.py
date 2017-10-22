3062ae1151edfd0ecdb9d7b6c4e20cc13cbebe90
pitsios-s <stamatis.pit@gmail.com>
2017-10-22 14:00:48 +0300

    Reformatted code for CNN script in single-digit cases

--- src/single_digit/cnn.py
+++ src/single_digit/cnn.py
@@ -110,111 +110,7 @@ def cnn(x):
     return tf.matmul(dropout_layer, weights["layer5"]) + biases["layer5"], keep_prob_constant
 
 
-# Build the graph for the deep net
-y_conv, keep_prob = cnn(X)
-
-# The cost function
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y_conv))
-
-# Optimizer used for training model
-optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-
-# Retrieve the indices of the best 3 predictions
-_, top_k_indices = tf.nn.top_k(y_conv, k=3)
-
-# Compare each of the best 3 predictions with the actual labels
-prediction_top_1 = tf.equal(top_k_indices[:, 0], tf.argmax(Y, 1, output_type=tf.int32))
-prediction_top_2 = tf.equal(top_k_indices[:, 1], tf.argmax(Y, 1, output_type=tf.int32))
-prediction_top_3 = tf.equal(top_k_indices[:, 2], tf.argmax(Y, 1, output_type=tf.int32))
-
-# Calculate accuracies for top-1, top-2 and top-3 predictions
-accuracy_top_1 = tf.reduce_mean(tf.cast(prediction_top_1, tf.float32))
-accuracy_top_2 = tf.reduce_mean(tf.cast(tf.reduce_any(
-    tf.stack([prediction_top_1, prediction_top_2]), axis=0), tf.float32))
-accuracy_top_3 = tf.reduce_mean(tf.cast(tf.reduce_any(
-    tf.stack([prediction_top_1, prediction_top_2, prediction_top_3]), axis=0), tf.float32))
-
-# Store scalars for accuracy and loss
-tf.summary.scalar('accuracy', accuracy_top_1)
-tf.summary.scalar('loss', cost)
-
-# Writers for storing tensorboard statistics
-merged = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter("../../logs/train")
-test_writer = tf.summary.FileWriter("../../logs/test")
-
-# Start counting execution time
-start_time = time.time()
-
-with tf.Session() as sess:
-    # Initialize Tensorflow variables
-    sess.run(tf.global_variables_initializer())
-
-    # Variables useful for batch creation
-    start = 0
-    end = 0
-
-    # The accuracy and loss for every iteration in train and test set
-    train_accuracies = []
-    train_losses = []
-    test_accuracies_top_1 = []
-    test_accuracies_top_2 = []
-    test_accuracies_top_3 = []
-    test_losses = []
-
-    for i in range(iterations):
-        # Construct the batch
-        if start == svhn.train_examples:
-            start = 0
-        end = min(svhn.train_examples, start + batch_size)
-
-        batch_x = svhn.train_data[start:end]
-        batch_y = svhn.train_labels[start:end]
-
-        start = end
-
-        # Run the optimizer
-        sess.run(optimizer, feed_dict={X: batch_x, Y: batch_y, keep_prob: dropout})
-
-        if (i + 1) % display_step == 0 or i == 0:
-            _accuracy_top_1, _accuracy_top_2, _accuracy_top_3, _cost, _summary = \
-                sess.run([accuracy_top_1, accuracy_top_2, accuracy_top_3, cost, merged],
-                         feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0})
-
-            print("Step: {0:6d}, "
-                  "Training Accuracy (Top 1): {1:5f}, "
-                  "Training Accuracy (Top 2): {2:5f}, "
-                  "Training Accuracy (Top 3): {3:5f}, "
-                  "Batch Loss: {4:5f}".format(i + 1, _accuracy_top_1, _accuracy_top_2, _accuracy_top_3, _cost))
-            train_accuracies.append(_accuracy_top_1)
-            train_losses.append(_cost)
-            train_writer.add_summary(_summary, i)
-
-    # Test the model by measuring it's accuracy
-    test_iterations = svhn.test_examples // batch_size + 1
-    for i in range(test_iterations):
-        batch_x, batch_y = (svhn.test_data[i * batch_size:(i + 1) * batch_size],
-                            svhn.test_labels[i * batch_size:(i + 1) * batch_size])
-        _accuracy_top_1, _accuracy_top_2, _accuracy_top_3, _cost, _summary = \
-            sess.run([accuracy_top_1, accuracy_top_2, accuracy_top_3, cost, merged],
-                     feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0})
-        test_accuracies_top_1.append(_accuracy_top_1)
-        test_accuracies_top_2.append(_accuracy_top_2)
-        test_accuracies_top_3.append(_accuracy_top_3)
-        test_losses.append(_cost)
-        test_writer.add_summary(_summary, i)
-    print("Mean Test Accuracy (Top 1): {0:5f}, "
-          "Mean Test Accuracy (Top 2): {1:5f}, "
-          "Mean Test Accuracy (Top 3): {2:5f}, "
-          "Mean Test Loss: {3:5f}".format(np.mean(test_accuracies_top_1),
-                                          np.mean(test_accuracies_top_2),
-                                          np.mean(test_accuracies_top_3),
-                                          np.mean(test_losses)))
-
-    # print execution time
-    print("Execution time in seconds: " + str(time.time() - start_time))
-    train_writer.close()
-
+def visualize_results(train_accuracies, train_losses, test_accuracies, test_losses, test_iterations):
     # Plot batch accuracy and loss for both train and test sets
     plt.style.use("ggplot")
     fig, ax = plt.subplots(2, 2)
@@ -238,7 +134,7 @@ with tf.Session() as sess:
     ax[1, 0].set_xlabel("Batch")
     ax[1, 0].set_ylabel("Accuracy")
     ax[1, 0].set_ylim([0, 1.05])
-    ax[1, 0].plot(range(0, test_iterations), test_accuracies_top_1, linewidth=1, color="darkgreen")
+    ax[1, 0].plot(range(0, test_iterations), test_accuracies, linewidth=1, color="darkgreen")
 
     # Test Loss
     ax[1, 1].set_title("Test Loss per Batch")
@@ -252,3 +148,115 @@ with tf.Session() as sess:
         ax[0, 1].axvline(x=i, ymin=0, ymax=max(train_losses), linewidth=2, color="orange")
 
     plt.show()
+
+
+def main():
+    # Build the graph for the deep net
+    y_conv, keep_prob = cnn(X)
+
+    # The cost function
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y_conv))
+
+    # Optimizer used for training model
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+    # Retrieve the indices of the best 3 predictions
+    _, top_k_indices = tf.nn.top_k(y_conv, k=3)
+
+    # Compare each of the best 3 predictions with the actual labels
+    prediction_top_1 = tf.equal(top_k_indices[:, 0], tf.argmax(Y, 1, output_type=tf.int32))
+    prediction_top_2 = tf.equal(top_k_indices[:, 1], tf.argmax(Y, 1, output_type=tf.int32))
+    prediction_top_3 = tf.equal(top_k_indices[:, 2], tf.argmax(Y, 1, output_type=tf.int32))
+
+    # Calculate accuracies for top-1, top-2 and top-3 predictions
+    accuracy_top_1 = tf.reduce_mean(tf.cast(prediction_top_1, tf.float32))
+    accuracy_top_2 = tf.reduce_mean(tf.cast(tf.reduce_any(
+        tf.stack([prediction_top_1, prediction_top_2]), axis=0), tf.float32))
+    accuracy_top_3 = tf.reduce_mean(tf.cast(tf.reduce_any(
+        tf.stack([prediction_top_1, prediction_top_2, prediction_top_3]), axis=0), tf.float32))
+
+    # Store scalars for accuracy and loss
+    tf.summary.scalar('accuracy', accuracy_top_1)
+    tf.summary.scalar('loss', cost)
+
+    # Writers for storing tensorboard statistics
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter("../../logs/train")
+    test_writer = tf.summary.FileWriter("../../logs/test")
+
+    # Start counting execution time
+    start_time = time.time()
+
+    with tf.Session() as sess:
+        # Initialize Tensorflow variables
+        sess.run(tf.global_variables_initializer())
+
+        # Variable useful for batch creation
+        start = 0
+
+        # The accuracy and loss for every iteration in train and test set
+        train_accuracies = []
+        train_losses = []
+        test_accuracies_top_1 = []
+        test_accuracies_top_2 = []
+        test_accuracies_top_3 = []
+        test_losses = []
+
+        for i in range(iterations):
+            # Construct the batch
+            if start == svhn.train_examples:
+                start = 0
+            end = min(svhn.train_examples, start + batch_size)
+
+            batch_x = svhn.train_data[start:end]
+            batch_y = svhn.train_labels[start:end]
+
+            start = end
+
+            # Run the optimizer
+            sess.run(optimizer, feed_dict={X: batch_x, Y: batch_y, keep_prob: dropout})
+
+            if (i + 1) % display_step == 0 or i == 0:
+                _accuracy_top_1, _accuracy_top_2, _accuracy_top_3, _cost, _summary = \
+                    sess.run([accuracy_top_1, accuracy_top_2, accuracy_top_3, cost, merged],
+                             feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0})
+
+                print("Step: {0:6d}, "
+                      "Training Accuracy (Top 1): {1:5f}, "
+                      "Training Accuracy (Top 2): {2:5f}, "
+                      "Training Accuracy (Top 3): {3:5f}, "
+                      "Batch Loss: {4:5f}".format(i + 1, _accuracy_top_1, _accuracy_top_2, _accuracy_top_3, _cost))
+                train_accuracies.append(_accuracy_top_1)
+                train_losses.append(_cost)
+                train_writer.add_summary(_summary, i)
+
+        # Test the model by measuring it's accuracy
+        test_iterations = svhn.test_examples // batch_size + 1
+        for i in range(test_iterations):
+            batch_x, batch_y = (svhn.test_data[i * batch_size:(i + 1) * batch_size],
+                                svhn.test_labels[i * batch_size:(i + 1) * batch_size])
+            _accuracy_top_1, _accuracy_top_2, _accuracy_top_3, _cost, _summary = \
+                sess.run([accuracy_top_1, accuracy_top_2, accuracy_top_3, cost, merged],
+                         feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0})
+            test_accuracies_top_1.append(_accuracy_top_1)
+            test_accuracies_top_2.append(_accuracy_top_2)
+            test_accuracies_top_3.append(_accuracy_top_3)
+            test_losses.append(_cost)
+            test_writer.add_summary(_summary, i)
+        print("Mean Test Accuracy (Top 1): {0:5f}, "
+              "Mean Test Accuracy (Top 2): {1:5f}, "
+              "Mean Test Accuracy (Top 3): {2:5f}, "
+              "Mean Test Loss: {3:5f}".format(np.mean(test_accuracies_top_1),
+                                              np.mean(test_accuracies_top_2),
+                                              np.mean(test_accuracies_top_3),
+                                              np.mean(test_losses)))
+
+        # print execution time
+        print("Execution time in seconds: " + str(time.time() - start_time))
+        train_writer.close()
+
+    visualize_results(train_accuracies, train_losses, test_accuracies_top_1, test_losses, test_iterations)
+
+
+if __name__ == '__main__':
+    main()
